25224fc996307c384af1a572d1c01febcdfd3d44
Christian Zangl <coralllama@gmail.com>
2014-06-28 13:55:02 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-gulp-hjson
-==========
+# gulp-hjson
+
+Hjson to/from JSON convert plugin for gulp 3.
+
+## Usage
+
+First, install `gulp-hjson` as a development dependency:
+
+```shell
+npm install --save-dev gulp-hjson
+```
+
+Then, add it to your `gulpfile.js`:
+
+```javascript
+var Hjson = require('gulp-hjson');
+
+gulp.task('json', function() {
+  gulp.src(['*.json'])
+    .pipe(Hjson({ to: 'hjson' }))
+    .pipe(gulp.dest('output'));
+});
+```
+
+## options
+
+### { to: 'json' }
+
+Convert to JSON.
+
+### { to: 'hjson' }
+
+Convert to Hjson.
+
---
+++ index.js
@@ -0,0 +1,55 @@
+var Hjson=require('hjson');
+var stream=require('stream');
+var path=require('path');
+var es=require('event-stream');
+var through=require('through');
+
+module.exports=function(options)
+{
+  options=options||{};
+
+  function convert(file, text, options)
+  {
+    var data=Hjson.parse(text);
+    var name=file.path.substr(0, file.path.length-path.extname(file.path).length);
+    switch (options.to)
+    {
+      case 'json':
+        file.path=name+".json";
+        return JSON.stringify(data);
+      case 'hjson':
+        file.path=name+".hjson";
+        return Hjson.stringify(data);
+      default: throw new Error('Please specify gulp-hjson options { to: "json"|"hjson" }!');
+    }
+  }
+
+  function processStream(file, options)
+  {
+    var text='';
+    function write(buf) { text+=buf.toString(options.encoding); }
+    function end() { this.queue(convert(file, text, options)); }
+    return through(write, end);
+  }
+
+  function process(file, cb)
+  {
+    var isStream=file.contents && typeof file.contents.on === 'function' && typeof file.contents.pipe === 'function';
+    var isBuffer=file.contents instanceof Buffer;
+
+    if (isStream)
+    {
+      file.contents=file.contents.pipe(processStream(file, options));
+      return cb(null, file);
+    }
+    else if (isBuffer)
+    {
+      var text=String(file.contents);
+      file.contents=new Buffer(convert(file, text, options));
+      return cb(null, file);
+    }
+    else cb(null, file);
+  }
+
+  return es.map(process);
+};
